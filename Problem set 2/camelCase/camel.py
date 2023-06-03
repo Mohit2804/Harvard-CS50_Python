dd8a4ade259043e3ea53bab7e0b47b79cb80b87b
Mohit G <56093151+Mohit2804@users.noreply.github.com>
2023-06-03 15:48:43 +0100

    Create camel.py

---
+++ Problem set 2/camelCase/camel.py
@@ -0,0 +1,11 @@
+camel = input("camelCase: ")
+print("snake_case: ", end="")
+
+
+for l in camel:
+    if l.isupper():
+        print("_" + l.lower(), end="")
+    else:
+        print(l, end="")
+
+print()
