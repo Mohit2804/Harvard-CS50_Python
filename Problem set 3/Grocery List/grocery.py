36739c2865cd290c91495039b348c1886bcc0e3d
Mohit G <56093151+Mohit2804@users.noreply.github.com>
2023-06-03 15:59:38 +0100

    Create grocery.py

---
+++ Problem set 3/Grocery List/grocery.py
@@ -0,0 +1,13 @@
+list = {}
+
+while True:
+    try:
+        item = input().lower()
+        if item in list:
+            list[item] += 1
+        else:
+            list[item] = 1
+    except EOFError:
+        for item in sorted(list.keys()):
+            print(list[item], item.upper())
+        break
