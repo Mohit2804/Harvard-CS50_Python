f1ee44200188f8f959c3c5c9c64975195b63d501
Mohit G <56093151+Mohit2804@users.noreply.github.com>
2023-06-03 16:26:13 +0100

    Create watch.py

---
+++ Problem set 7/Watch on YouTube/watch.py
@@ -0,0 +1,15 @@
+import re
+
+def main():
+    print(parse(input("HTML: ")))
+
+def parse(s):
+    if re.search(r"<iframe(.)*><\/iframe>", s):
+        url = re.search(r"(http(s)?:\/\/(?:www\.)?youtube\.com\/embed\/)([a-z_A-Z_0-9]+)", s)
+        if url:
+            split_url = url.groups()
+            return "https://youtu.be/" + split_url[2]
+
+
+if __name__ == "__main__":
+    main()
